ac5d799c9942f6341a8549c1a7c6797208c0fd1a
limkyulee <mud0308@gmail.com>
2025-04-04 11:18:23 +0900

    add gsap playground to, from example

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -19,8 +19,8 @@ const moveToPage = (path: string) => {
     <h1 class="mb-5"><b>&#10043; Chapter</b></h1>
     <div class="flex justify-start gap-5">
       <button class="button" @click="moveToPage('/infinity')">Scroll Interaction &#10024;</button>
-      <button class="button" @click="moveToPage('/web-gl')">WebGL</button>
-      <button class="button" disabled @click="moveToPage('/gsap')">Gsap (Upcoming)</button>
+      <button class="button" @click="moveToPage('/web-gl')">WebGL &#10024;</button>
+      <button class="button" @click="moveToPage('/gsap')">Gsap &#10024;</button>
     </div>
   </div>
 </template>
