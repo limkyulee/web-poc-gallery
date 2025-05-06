b3e830215295255105bbb1841edddfb151acf22b
limkyulee <mud0308@gmail.com>
2025-05-06 20:25:41 +0900

    add jwt composable file

---
+++ src/composables/auth/jwt.ts
@@ -0,0 +1,19 @@
+const KEY = {
+  KEY_ACCESS_TOKEN: "KEY_ACCESS_TOKEN"
+} as const
+
+const getAccessToken = () => {
+  return localStorage.getItem(KEY.KEY_ACCESS_TOKEN)
+}
+
+const setAccessToken = (accessToken: string) => {
+  localStorage.setItem(KEY.KEY_ACCESS_TOKEN, accessToken)
+}
+
+const clearAccessToken = () => {
+  localStorage.removeItem(KEY.KEY_ACCESS_TOKEN)
+}
+
+export const setToken = (accessToken: string) => {
+  setAccessToken(accessToken)
+}
