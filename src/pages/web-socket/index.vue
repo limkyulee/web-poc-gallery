6132f641b1608d229cc26dc9ec20482f73795a2d
limkyulee <mud0308@gmail.com>
2025-05-06 23:15:36 +0900

    feat socket p ui

---
+++ src/pages/web-socket/index.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const connectStatus = ref<boolean>(false)
+const messages = ref<string[]>(['s','s','s'])
+
+const handleConnectStatus = () => {
+  connectStatus.value = !connectStatus.value
+}
+</script>
+
+<template>
+  <div>
+    <div class="socket-label--wrapper">
+      <div class="socket-label">
+        <label>WebSocket Connection</label>
+        <button class="button button--outline">Connect</button>
+        <button class="button button--outline">Disconnect</button>
+      </div>
+      <div class="socket-label">
+        <label>What's your name?</label>
+        <input type="text" placeholder="Enter your name" class="input" />
+        <button class="button button--outline">Send</button>
+      </div>
+    </div>
+    <div class="socket-table--wrapper">
+      <table class="socket-table">
+        <thead>
+          <tr>
+            <th>Message</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(msg, index) in messages" :key="index">
+            <td>{{ msg }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
